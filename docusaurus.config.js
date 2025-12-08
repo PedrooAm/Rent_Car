1bc0bead9bbd46498e16cf95c93a4f630ad0f3b7
Pedro <2014pedroamaro@gmail.com>
2025-12-08 20:46:06 +0000

    alterei as configs do docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,6 +14,15 @@ const config = {
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
+  url: 'https://github.com/',
+  baseUrl: '/Rent_Car',
+  organizationName: 'PedrooAm',
+  projectName: 'Rent_Car',
+  deploymentBranch: 'gh-pages',
+  trailingSlash:false,
+
+
+
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
